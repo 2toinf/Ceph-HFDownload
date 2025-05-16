61f67e6c54722b43f4c056dc5b3ba9a7d35c84e4
Jinliang Zheng <54488861+2toinf@users.noreply.github.com>
2025-05-16 14:18:52 +0800

    Create slurm_submit.sh

---
+++ slurm_submit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# === 传参：group 文件 ===
+GROUP_FILE="$1"
+
+# 环境变量从外部传入
+# REPO_ID, REMOTE, FILE_LIST_PATH 必须提前 export
+
+while IFS= read -r PREFIX || [[ -n "$PREFIX" ]]; do
+    bash ./hf_download.sh "$PREFIX"
+done < "$GROUP_FILE"
