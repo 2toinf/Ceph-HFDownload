3c51312e8664c753b0ec1ab66354b9cbfc72361b
Jinliang Zheng <54488861+2toinf@users.noreply.github.com>
2025-05-16 14:38:43 +0800

    Update submit_all_batch.sh

--- submit_all_batch.sh
+++ submit_all_batch.sh
@@ -4,7 +4,7 @@
 
 for GROUP_FILE in group_batch_*; do
     echo "[$i] srun 启动批任务：$GROUP_FILE"
-    srun -p mozi-S1 -N1 bash ./slurm_download.sh "$GROUP_FILE" &
+    srun -p mozi-S1 -N1 bash ./slurm_submit.sh "$GROUP_FILE" &
     sleep 1s
 done
 
